8fddda8c7534f855ec9362a86bc6ac18c5ab61c7
martina <mesler478@gmail.com>
2024-06-24 22:27:35 -0300

    se creo otra carpeta template dentro d inicio- se agrego plantilla alproyecto

--- inicio/views.py
+++ inicio/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse 
+from django.shortcuts import render 
 from datetime import datetime
 
+def inicio (request):
+    return render (request, 'inicio/index.html')
