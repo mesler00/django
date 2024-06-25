2ce2523fe62bebb7dc34ac426d4ef271e662dc07
martina <mesler478@gmail.com>
2024-06-24 22:35:50 -0300

    se creo el modelo cliente

--- inicio/models.py
+++ inicio/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class cliente :
+    nombre_apellido= models.CharField (max_length= 50)
+    email= models.CharField (max_length= 20)
+    ingreso= models.CharField (max_length= 15)
+    
